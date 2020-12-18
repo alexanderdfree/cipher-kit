f8b248000ccf216689e31d18533bbae5f7967b47
Hippoman <41807606+1Hippoman1@users.noreply.github.com>
2020-12-18 03:13:38 -0800

    finished decryptor
    whew easy

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -24,6 +24,30 @@ public class CaesarCipher
         }
         return plaintext;
     }
+    public static String Decryptor(String code, int offset2)
+    {
+        /*Scanner scanXD = new Scanner(System.in);
+        System.out.print("What message would you like to decrypt? ");
+        String ciphertext = scanXD.nextLine();
+        System.out.print("What is the key for the cipher? ");
+        Scanner scanXD2 = new Scanner(System.in);*/
+        int offset = offset2 % 26;
+        String plaintext = "";
+        for (int i = 0; i < code.length(); i++) {
+            String c1 = code.substring(i, i+1);
+            int realid = 0;
+            char c = c1.charAt(0);
+            if (c <= 'z' && c >= 'a') {
+               realid = 97 + (26-Math.abs(c-97 - offset)%26);
+            }
+            else if (c <= 'Z' && c >= 'A') {
+               realid = 65 + (26-Math.abs(c-65 - offset)%26);
+            }
+            char realchar = (char)realid;
+            plaintext += realchar;
+        }
+        return plaintext;
+    }
 
 }
 
